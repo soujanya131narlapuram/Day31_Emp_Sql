4499771c94185b1d6911111335d372c012047bcc
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-22 13:40:44 +0530

    'UC4 Commit'

--- src/main/java/com/bridgelabz/Query.sql
+++ src/main/java/com/bridgelabz/Query.sql
@@ -1,5 +1,14 @@
 use payroll_service;
-insert into employee_payroll(name,salary, start)
+
+CREATE TABLE employee_payroll (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    salary DOUBLE NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+insert into employee_payroll(name,salary,start)
 values("Soujanya",6000000.0,'2021-10-22'),
 ("Ashok",10000000.0,'2019-01-22'),
 ("Sravan",9000000.0,'2022-10-22'),
